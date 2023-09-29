a0a742c61f548097bb8f461a2f72712aa45c7b95
Ezra4no1 <65947098+Ezra4no1@users.noreply.github.com>
2023-09-29 08:20:50 -0600

    Add files via upload
    <# This script is used to Ping Test a Server node and get
    its FQDN and then output the results to a CSV file. #>

---
+++ Get-IP-FQDN-PingTest.ps1
@@ -0,0 +1,58 @@
+﻿cls
+
+<# This script is used to Ping Test a Server node and get
+its FQDN and then outputs the results to a csv file. #>
+
+
+$servers = Get-Content -Path "C:\PATH_TO_LOCATION\FILE_NAME.txt"
+$results = @()
+
+foreach ($server in $servers) {
+    $serverName = $server
+    $fqdn = ""
+    $ipAddress = ""
+    $pingResponse = ""
+
+ 
+
+    # Get FQDN and IP address
+    try {
+        $fqdn = [System.Net.Dns]::GetHostEntry($server).HostName
+        $ipAddress = [System.Net.Dns]::GetHostEntry($server).AddressList.IPAddressToString
+    } catch {
+        $fqdn = "Unknown"
+        $ipAddress = "Unknown"
+    }
+
+ 
+
+    # Ping test
+    $pingReply = Test-Connection -ComputerName $server -Count 1 -Quiet
+    if ($pingReply) {
+        $pingResponse = "Responded"
+    } else {
+        $pingResponse = "Not Responded"
+    }
+
+ 
+
+    # Create custom object with server details
+    $serverDetails = [PSCustomObject]@{
+        ServerName = $serverName
+        FQDN = $fqdn
+        IPAddress = $ipAddress
+        PingResponse = $pingResponse
+    }
+    $results += $serverDetails
+
+ 
+
+    # Display countdown on the console
+    $serversLeft = $servers.Count - $results.Count
+    Write-Host "Servers left to check: $serversLeft"
+}
+
+ 
+
+# Export results to CSV file
+$results | Export-Csv "C:\PATH_TO_LOCATION\FILE_NAME.csv" -NoTypeInformation
