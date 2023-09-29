2d54c0ddcfb4938028b144f54a0c288cd75c3f48
Ezra4no1 <65947098+Ezra4no1@users.noreply.github.com>
2023-09-29 10:17:37 -0600

    Add files via upload
    <# This script checks to see if RDS is enabled on remote servers #>

---
+++ PS_CheckRDSenabled.ps1
@@ -0,0 +1,47 @@
+﻿CLS
+
+<# This script checks to see if RDS is enabled on remote servers #>
+
+$serverListFile = "C:\PATH_TO_LOCATION\PS_CheckRDSenabled-INPUT.txt"
+$outputCSV = "C:\PATH_TO_LOCATION\CheckRDSenabled-OUTPUT.csv"
+
+ 
+
+# Read the server list from the text file
+$servers = Get-Content $serverListFile
+$totalServers = $servers.Count
+$currentServer = 1
+
+ 
+
+# Create an array to store the results
+$results = @()
+
+ 
+
+foreach ($server in $servers) {
+    # Check if RDS is enabled on the remote server
+    $rdsEnabled = Test-WSMan -ComputerName $server -ErrorAction SilentlyContinue
+
+    # Create an object with the server name and RDS status
+    $result = [PSCustomObject]@{
+        ServerName = $server
+        RDSStatus = $rdsEnabled
+    }
+
+    # Add the result to the array
+    $results += $result
+
+    # Display the countdown on the screen
+    Write-Host "Servers left to check: $($totalServers - $currentServer)"
+    $currentServer++
+}
+
+ 
+
+# Export the results to a CSV file
+$results | Export-Csv -Path $outputCSV -NoTypeInformation
+
+ 
+
+Write-Host "Script execution completed."
